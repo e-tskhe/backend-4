348fdf635f895cece46338d5e4e7a9d9c723a1e8
e-tskhe <youngartist7009@gmail.com>
2025-05-11 18:05:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -65,137 +65,138 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     include('form.php');
     exit();
 }
-
-$errors = FALSE;
-
-if (empty($_POST['name'])) {
-    setcookie('name_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} elseif (!preg_match('/^[а-яёa-z\s-]+$/iu', $_POST['name'])) {
-    setcookie('name_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('name_value', $_POST['name'], time() + 365 * 24 * 60 * 60);
-}
-
-if (empty($_POST['phone'])) {
-    setcookie('phone_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} elseif (!preg_match('/^(\+7|7|8)\d{10}$/', $_POST['phone'])) {
-    setcookie('phone_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('phone_value', $_POST['phone'], time() + 365 * 24 * 60 * 60);
-}
-
-if (empty($_POST['email'])) {
-    setcookie('email_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    setcookie('email_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('email_value', $_POST['email'], time() + 365 * 24 * 60 * 60);
-}
-
-if (empty($_POST['birthdate'])) {
-    setcookie('birthdate_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('birthdate_value', $_POST['birthdate'], time() + 365 * 24 * 60 * 60);
-}
-
-if (empty($_POST['gender'])) {
-    setcookie('gender_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('gender_value', $_POST['gender'], time() + 365 * 24 * 60 * 60);
-}
-
-if (empty($_POST['languages'])) {
-    setcookie('languages_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('languages_value', serialize($_POST['languages']), time() + 365 * 24 * 60 * 60);}
-
-if (empty($_POST['bio'])) {
-    setcookie('bio_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} elseif (!preg_match('/^[а-яёa-z0-9\s.,!?-]+$/iu', $_POST['bio'])) {
-    setcookie('bio_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('bio_value', $_POST['bio'], time() + 365 * 24 * 60 * 60);
-}
-
-if (empty($_POST['agreement'])) {
-    setcookie('agreement_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-} else {
-    setcookie('agreement_value', $_POST['agreement'], time() + 365 * 24 * 60 * 60);
-}
-
-if ($errors) {
-    header('Location: index.php');
-    exit();
-}
-
-setcookie('name_error', '', 100000);
-setcookie('phone_error', '', 100000);
-setcookie('email_error', '', 100000);
-setcookie('birthdate_error', '', 100000);
-setcookie('gender_error', '', 100000);
-setcookie('languages_error', '', 100000);
-setcookie('bio_error', '', 100000);
-setcookie('agreement_error', '', 100000);
-
-$user = 'u68891'; 
-$pass = '3849293'; 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=u68891', $user, $pass,
-        [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-    $pdo->beginTransaction();
-
-    $stmt = $pdo->prepare("INSERT INTO application (name, phone, email, birthdate, gender, bio) 
-              VALUES (:name, :phone, :email, :birthdate, :gender, :bio)");
-    $stmt->execute([
-        ':name' => $_POST['name'],
-        ':phone' => $_POST['phone'],
-        ':email' => $_POST['email'],
-        ':birthdate' => $_POST['birthdate'],
-        ':gender' => $_POST['gender'],
-        ':bio' => $_POST['bio']
-    ]);
+else {
+    $errors = FALSE;
     
-    $applicationId = $pdo->lastInsertId();
-
-    $langStmt = $pdo->prepare("
-        INSERT INTO programming_language (name)
-        VALUES (:name)
-        ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)
-    ");
-    
-    $appLangStmt = $pdo->prepare("
-        INSERT INTO application_language (application_id, language_id)
-        VALUES (:app_id, :lang_id)
-    ");
-    
-    foreach ($_POST['languages'] as $langName) {
-        $langStmt->execute([':name' => $langName]);
-        $langId = $pdo->lastInsertId();
-        $appLangStmt->execute([
-            ':app_id' => $applicationId,
-            ':lang_id' => $langId
-        ]);
+    if (empty($_POST['name'])) {
+        setcookie('name_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } elseif (!preg_match('/^[а-яёa-z\s-]+$/iu', $_POST['name'])) {
+        setcookie('name_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('name_value', $_POST['name'], time() + 365 * 24 * 60 * 60);
     }
     
-    $pdo->commit();
+    if (empty($_POST['phone'])) {
+        setcookie('phone_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } elseif (!preg_match('/^(\+7|7|8)\d{10}$/', $_POST['phone'])) {
+        setcookie('phone_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('phone_value', $_POST['phone'], time() + 365 * 24 * 60 * 60);
+    }
     
-    setcookie('save', '1');
-    header('Location: index.php');
-} catch (PDOException $e) {
-    $pdo->rollBack();
-    print('Error: ' . $e->getMessage());
-    exit();
+    if (empty($_POST['email'])) {
+        setcookie('email_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        setcookie('email_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('email_value', $_POST['email'], time() + 365 * 24 * 60 * 60);
+    }
+    
+    if (empty($_POST['birthdate'])) {
+        setcookie('birthdate_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('birthdate_value', $_POST['birthdate'], time() + 365 * 24 * 60 * 60);
+    }
+    
+    if (empty($_POST['gender'])) {
+        setcookie('gender_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('gender_value', $_POST['gender'], time() + 365 * 24 * 60 * 60);
+    }
+    
+    if (empty($_POST['languages'])) {
+        setcookie('languages_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('languages_value', serialize($_POST['languages']), time() + 365 * 24 * 60 * 60);}
+    
+    if (empty($_POST['bio'])) {
+        setcookie('bio_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } elseif (!preg_match('/^[а-яёa-z0-9\s.,!?-]+$/iu', $_POST['bio'])) {
+        setcookie('bio_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('bio_value', $_POST['bio'], time() + 365 * 24 * 60 * 60);
+    }
+    
+    if (empty($_POST['agreement'])) {
+        setcookie('agreement_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('agreement_value', $_POST['agreement'], time() + 365 * 24 * 60 * 60);
+    }
+    
+    if ($errors) {
+        header('Location: index.php');
+        exit();
+    }
+    
+    setcookie('name_error', '', 100000);
+    setcookie('phone_error', '', 100000);
+    setcookie('email_error', '', 100000);
+    setcookie('birthdate_error', '', 100000);
+    setcookie('gender_error', '', 100000);
+    setcookie('languages_error', '', 100000);
+    setcookie('bio_error', '', 100000);
+    setcookie('agreement_error', '', 100000);
+    
+    $user = 'u68891'; 
+    $pass = '3849293'; 
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=u68891', $user, $pass,
+            [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    
+        $pdo->beginTransaction();
+    
+        $stmt = $pdo->prepare("INSERT INTO application (name, phone, email, birthdate, gender, bio) 
+                  VALUES (:name, :phone, :email, :birthdate, :gender, :bio)");
+        $stmt->execute([
+            ':name' => $_POST['name'],
+            ':phone' => $_POST['phone'],
+            ':email' => $_POST['email'],
+            ':birthdate' => $_POST['birthdate'],
+            ':gender' => $_POST['gender'],
+            ':bio' => $_POST['bio']
+        ]);
+        
+        $applicationId = $pdo->lastInsertId();
+    
+        $langStmt = $pdo->prepare("
+            INSERT INTO programming_language (name)
+            VALUES (:name)
+            ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)
+        ");
+        
+        $appLangStmt = $pdo->prepare("
+            INSERT INTO application_language (application_id, language_id)
+            VALUES (:app_id, :lang_id)
+        ");
+        
+        foreach ($_POST['languages'] as $langName) {
+            $langStmt->execute([':name' => $langName]);
+            $langId = $pdo->lastInsertId();
+            $appLangStmt->execute([
+                ':app_id' => $applicationId,
+                ':lang_id' => $langId
+            ]);
+        }
+        
+        $pdo->commit();
+        
+        setcookie('save', '1');
+        header('Location: index.php');
+    } catch (PDOException $e) {
+        $pdo->rollBack();
+        print('Error: ' . $e->getMessage());
+        exit();
+    }
 }
